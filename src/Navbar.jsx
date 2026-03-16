e77f95fc92949700281cd0ae7ee0d269d891af8f
Aniket5353 <>
2026-03-16 17:50:49 +0530

    new
    mew

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import logo2 from "./assets/logo2.png";
+import logo2 from "./assets/halicoptor.png";
 
 function Navbar() {
     const [open, setOpen] = useState(false);
